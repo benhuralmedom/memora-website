f6d20a2f1e8958285d7c68c1b7478e09f702680e
Benhur Almedom <benhur.almedom@shidanetwork.com>
2025-06-28 19:07:00 +0200

    Fixa om oss sida

--- src/app/om-oss/page.tsx
+++ src/app/om-oss/page.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+import styles from "./om-oss.module.css";
+
 export default function OmOssPage() {
-  return (
-    <div style={{ padding: "20rem" }}>
-      <h1>Om oss</h1>
-      <p>Välkommen till Memora. Här kan du lära dig mer om oss.</p>
-    </div>
+    return (
+    <div className={styles.container}>
+        <div className={styles.backgroundEllipse} />
+        <h1 className={styles.mission}>
+            Vår mission:
+            <br />
+            Gör kunskap tillgänglig
+            </h1>
+            <div className={styles.description}>
+                <p>Vi är AI-specialister med mångårig erfarenhet från tech och enterprise-lösningar.</p>
+                <p>Vårt mål är att förenkla intern informationssökning och öka produktiviteten.</p>
+                </div>
+         </div>
   );
-}
+  }
