5359910d22844b785ca2661b8c442fd76f299326
Naweed Khushal <naweed.khushal@shidanetwork.com>
2025-06-28 21:58:41 +0200

    removed dead code, now cleaner code

--- src/app/hem.tsx
+++ src/app/hem.tsx
@@ -8,36 +8,34 @@ import KeyFeatures from "../components/KeyFeatures";
 const Hem: NextPage = () => {
   return (
     <div className={styles.hem}>
-      <div className={styles.splineLeft}>
-        <Spline scene="https://prod.spline.design/l6fJ31p8NzHsHjN2/scene.splinecode" />
-      </div>
-      
-      <div className={styles.hemChild} />
-      <div className={styles.hemItem} />
-      <div className={styles.ellipseDiv} />
-      <b className={styles.allKunskapDirektContainer}>
-        <p className={styles.allKunskap}>All Company Knowledge.</p>
-        <p className={styles.allKunskap}>One Interface.</p>
-      </b>
-      <div className={styles.anslutDinaDokumentContainer}>
-        <p className={styles.allKunskap}>
-          Memora is an AI that knows everything about your company - and feeds you answers in real time.
-        </p>
-      </div>
+      <section className={styles.heroSection}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.titleContainer}>
+              All Company Knowledge.<br />
+              One Interface.
+          </h1>
+          <p className={styles.titleContainerContainer}>
+            Memora is an AI that knows everything about your company - and feeds you answers in real time.
+          </p>
+          <div className={styles.buttonContainer}>
+            <div className={styles.component7}>
+              <div className={styles.bokaDemo}>BOKA DEMO</div>
+            </div>
+            <div className={styles.component8}>
+              <div className={styles.seFunktioner}>SE FUNKTIONER</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.splineLeft}>
+          <Spline scene="https://prod.spline.design/l6fJ31p8NzHsHjN2/scene.splinecode" />
+        </div>
+      </section>
       <div className={styles.vlkommenTillFramtiden}>
-          Teams waste 30% of their time searching for information.<br/>
-          Memora gives that time back.
+        Teams waste 30% of their time searching for information.<br />
+        Memora gives that time back.
       </div>
       <div className={styles.sFungerarDet}>Key Features</div>
       <KeyFeatures />
-      <div className={styles.buttonContainer}>
-        <div className={styles.component7}>
-          <div className={styles.bokaDemo}>BOKA DEMO</div>
-        </div>
-        <div className={styles.component8}>
-          <div className={styles.seFunktioner}>SE FUNKTIONER</div>
-        </div>
-      </div>
     </div>
   );
 };
